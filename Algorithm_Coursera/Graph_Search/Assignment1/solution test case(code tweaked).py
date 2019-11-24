4ffc1479cd24cc259789133272d760f539f46a1c
Sijoon Lee <shijoonlee@gmail.com>
2019-11-23 18:55:31 -0800

    update: graph assign1

--- Algorithm_Coursera/Graph_Search/Assignment1/solution test case(code tweaked).py
+++ Algorithm_Coursera/Graph_Search/Assignment1/solution test case(code tweaked).py
@@ -77,6 +77,7 @@ def scc_finder(seedNo):
     #                     if len(gr[head]) == 0:
     #                         stack.append(head)
     
+    print("node order in 1st DFS", nodes)
     for node in nodes:
         if visited[node] == True:
             continue
@@ -103,6 +104,7 @@ def scc_finder(seedNo):
     stack = []
     # order = order[1:] # delete node 0 which doesn't exist
     order.reverse()  # The nodes should be visited in reverse finishing times
+    print("order in 2nd DFS", order)
     for node in order:
         if visited_scc[node] == True:
             continue
@@ -162,27 +164,70 @@ if __name__ == "__main__":
             len_array.append(len(arr))
         totalScc = len(len_array)
         len_array.sort(reverse=True)
+        
+        print("scc", scc)
         print("len", len_array[:5])
         print("seed",seed, "total # SCCs",totalScc)
+        print("####################################")
 
 
+# node order in 1st DFS [1, 9, 8, 5, 10, 2, 3, 7, 4, 0, 11, 6]
+# order in 2nd DFS [6, 10, 11, 8, 2, 4, 7, 9, 3, 5, 1]
+# scc defaultdict(<class 'list'>, {1: [6, 10, 8], 2: [11], 3: [2, 4, 7, 9], 4: [3, 5, 1]})
 # len [4, 3, 3, 1]
 # seed 0 total # SCCs 4
+# ####################################
+# node order in 1st DFS [7, 11, 0, 8, 5, 6, 3, 10, 4, 1, 9, 2]
+# order in 2nd DFS [6, 10, 8, 11, 2, 9, 4, 1, 3, 5, 7]
+# scc defaultdict(<class 'list'>, {0: [11], 2: [6, 10, 8], 3: [2, 4, 7, 9], 4: [1, 3, 5]})
 # len [4, 3, 3, 1]
 # seed 1 total # SCCs 4
+# ####################################
+# node order in 1st DFS [9, 11, 3, 4, 7, 6, 8, 2, 5, 10, 1, 0]
+# order in 2nd DFS [10, 8, 6, 11, 2, 4, 1, 3, 5, 7, 9]
+# scc defaultdict(<class 'list'>, {0: [11], 2: [10, 8, 6], 3: [2, 4, 7, 9], 4: [1, 3, 5]})
 # len [4, 3, 3, 1]
 # seed 2 total # SCCs 4
+# ####################################
+# node order in 1st DFS [1, 7, 10, 0, 6, 11, 4, 5, 2, 8, 9, 3]
+# order in 2nd DFS [8, 11, 6, 10, 2, 9, 4, 7, 3, 5, 1]
+# scc defaultdict(<class 'list'>, {1: [8, 6, 10], 2: [11], 3: [2, 4, 7, 9], 4: [3, 5, 1]})
 # len [4, 3, 3, 1]
 # seed 3 total # SCCs 4
+# ####################################
+# node order in 1st DFS [11, 2, 8, 10, 5, 0, 9, 7, 6, 1, 4, 3]
+# order in 2nd DFS [6, 10, 8, 4, 7, 9, 2, 5, 1, 3, 11]
+# scc defaultdict(<class 'list'>, {0: [11], 2: [6, 10, 8], 3: [4, 7, 9, 2], 4: [5, 1, 3]})
 # len [4, 3, 3, 1]
 # seed 4 total # SCCs 4
+# ####################################
+# node order in 1st DFS [10, 2, 11, 7, 1, 3, 6, 0, 8, 5, 4, 9]
+# order in 2nd DFS [2, 4, 7, 9, 8, 5, 1, 3, 11, 6, 10]
+# scc defaultdict(<class 'list'>, {1: [2, 10, 8, 6, 4, 7, 9], 2: [5, 1, 3, 11]})
 # len [7, 4]
 # seed 5 total # SCCs 2
+# ####################################
+# node order in 1st DFS [6, 5, 2, 3, 8, 10, 11, 0, 4, 7, 1, 9]
+# order in 2nd DFS [4, 7, 9, 2, 10, 8, 5, 1, 3, 11, 6]
+# scc defaultdict(<class 'list'>, {1: [4, 7, 9, 8, 6, 10, 2], 2: [5, 1, 3, 11]})
 # len [7, 4]
 # seed 6 total # SCCs 2
+# ####################################
+# node order in 1st DFS [7, 11, 3, 10, 8, 4, 9, 1, 0, 6, 2, 5]
+# order in 2nd DFS [8, 6, 10, 11, 2, 9, 4, 1, 3, 5, 7]
+# scc defaultdict(<class 'list'>, {0: [11], 2: [8, 6, 10], 3: [2, 4, 7, 9], 4: [1, 3, 5]})
 # len [4, 3, 3, 1]
 # seed 7 total # SCCs 4
+# ####################################
+# node order in 1st DFS [7, 8, 4, 1, 9, 0, 10, 11, 2, 6, 5, 3]
+# order in 2nd DFS [6, 10, 11, 8, 2, 9, 4, 1, 3, 5, 7]
+# scc defaultdict(<class 'list'>, {1: [6, 10, 8], 2: [11], 3: [2, 4, 7, 9], 4: [1, 3, 5]})
 # len [4, 3, 3, 1]
 # seed 8 total # SCCs 4
+# ####################################
+# node order in 1st DFS [8, 6, 3, 11, 0, 10, 1, 2, 4, 5, 9, 7]
+# order in 2nd DFS [4, 7, 9, 2, 6, 10, 5, 1, 3, 11, 8]
+# scc defaultdict(<class 'list'>, {1: [4, 7, 9, 8, 6, 10, 2], 2: [5, 1, 3, 11]})
 # len [7, 4]
 # seed 9 total # SCCs 2
+# ####################################
